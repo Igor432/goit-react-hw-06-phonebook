e4a68f44409da478037e066c485d716a88d7483e
Igor432 <igomar0602@gmail.com>
2022-07-31 15:59:14 +0300

    phonebook normalized

--- src/components/phonebook/phonebooks.jsx
+++ src/components/phonebook/phonebooks.jsx
@@ -2,7 +2,9 @@ import { Component } from "react";
 import { nanoid } from 'nanoid'
 import '../phonebook/phonebook.css'
 
+
 class Phonebook extends Component {
+
 
     state = {
         contacts: [],
@@ -10,72 +12,91 @@ class Phonebook extends Component {
         number: ''
       }
 
+
+handleChange = (e) => {
+const target = e.target
+const name = target.name
+const value = target.value
+
+this.setState({[name]: value})
+
+
+}
+
+
     onSubmit = (e) => {
         e.preventDefault();
-       const form = e.currentTarget
-       const nameValue = form.elements.name.value
-       const numberValue = form.elements.number.value
-       this.setState({name: nameValue, contacts: [...this.state.contacts, nameValue]})
+        const list = document.querySelector('contacts_list')
+        const {name, number} = this.state
+        const {contacts} = this.state
+        
+       this.setState({contacts: [...contacts, {id: nanoid(), name: name, number: number}] }, console.log(this.state.contacts))
+       
 
-console.log(this.state.number)
+       
     }
 
-    
-   
+
+
     makeList = (key) =>  {
+        
         return (
             <ul class='contacts_list'>
     
        { key.map(contact => (
-            <li key={contact} class='contact_item'>{contact}</li>
+            <li key={contact.id} class='contact_item'>{contact.name} <br />{contact.number}</li>
     
         ))}
     
         </ul>
         )
     }
+
+
+
     
-
-
-
-
-
 render () {
+
+    
   
 const {contacts} = this.state
     
 
     return (
-
+<div class="main_div">
 <form onSubmit={this.onSubmit} class='forma'>
+    <p>Name</p>
     <input
     type="text"
     name="name"
     pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
     title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
     required
+  onChange={this.handleChange}
   />
+  <p>Number</p>
   <input
   type="tel"
   name="number"
   pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
   title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
   required
+  onChange={this.handleChange}
 />
 
   <button type="submit" class='submit_button'>Add Contact</button>
-  <div class='contacts_list'>{this.makeList(contacts)}</div>
 </form>
+<div class='contacts'>
+   {this.makeList(this.state.contacts)}
+</div>
+
+</div>
     )
 }
 
-
-
-
-
-
-
 }
+
+
 
 
 
